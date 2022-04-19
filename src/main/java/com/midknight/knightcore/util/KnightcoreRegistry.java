8894099f9f12a37f2d945af718d4edcc24c73613
Midknight-Maxielle <midknight.maxielle@gmail.com>
2022-04-18 21:58:34 -0400

    Updating to 1.18.2 / Forge 40

---
+++ src/main/java/com/midknight/knightcore/util/KnightcoreRegistry.java
@@ -0,0 +1,34 @@
+package com.midknight.knightcore.util;
+
+import com.midknight.knightcore.Knightcore;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class KnightcoreRegistry {
+
+    protected static IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+    // Deferred Registers
+
+    public static DeferredRegister<EntityType<?>> ENTITY_TYPES =
+        DeferredRegister.create(ForgeRegistries.ENTITIES, Knightcore.MOD_ID);
+
+    public static final DeferredRegister<Item> ITEM =
+    DeferredRegister.create(ForgeRegistries.ITEMS, Knightcore.MOD_ID);
+
+    // - - - - - - - - - //
+    // Registry Objects  //
+    // - - - - - - - - - //
+
+    // Arrow & Quivers Item //
+
+
+    // Register Methods
+
+    public static void registerITEM() {ITEM.register(eventBus);}
+}
